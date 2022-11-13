56fbc7569f96ca15750a16118410d4f5e6314ada
112719Carrion <francocarrion09@hotmail.com>
2022-11-12 21:19:18 -0300

    Graficos listos

--- src/app/reportes/reportes.component.ts
+++ src/app/reportes/reportes.component.ts
@@ -1,96 +1,178 @@
 import { Component, OnInit } from '@angular/core';
-import { ChartData } from 'chart.js';
+import { Chart, ChartData, registerables } from 'chart.js';
+import { ReportesService } from '../services/reportes.service';
 
 @Component({
   selector: 'app-reportes',
   templateUrl: './reportes.component.html',
-  styleUrls: ['./reportes.component.css']
+  styleUrls: ['./reportes.component.css'],
 })
 export class ReportesComponent implements OnInit {
+  charDataBarras: any;
+  charDataVictorias: any;
+  charDataVictorias21: any;
 
-  datos: ChartData<'bar', number[], string> = {
-    labels: ['Domingo', 'Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes', 'Sabado'],
-    datasets: [
-      {
-        label: 'Partidas jugadas',
-        data: [65, 59, 80, 81, 56, 55, 40],
-        backgroundColor: [
-          'rgba(255, 99, 132, 0.2)',
-          'rgba(54, 162, 235, 0.2)',
-          'rgba(255, 206, 86, 0.2)',
-          'rgba(75, 192, 192, 0.2)',
-          'rgba(153, 102, 255, 0.2)',
-          'rgba(255, 159, 64, 0.2)'
-        ],
-        borderColor: [
-          'rgba(255, 99, 132, 1)',
-          'rgba(54, 162, 235, 1)',
-          'rgba(255, 206, 86, 1)',
-          'rgba(75, 192, 192, 1)',
-          'rgba(153, 102, 255, 1)',
-          'rgba(255, 159, 64, 1)'
-        ],
-        borderWidth: 1,
-      },
-    ],
-  };
-  datosTorta: ChartData<'doughnut', number[], string> = {
-    labels: ['Jugador', 'Crupier'],
-    datasets: [
-      {
-        label: 'Proporcion de victorias',
-        data: [65, 59],
-        backgroundColor: [
-          'rgba(255, 99, 132, 0.2)',
-          'rgba(54, 162, 235, 0.2)',
-          'rgba(255, 206, 86, 0.2)',
-          'rgba(75, 192, 192, 0.2)',
-          'rgba(153, 102, 255, 0.2)',
-          'rgba(255, 159, 64, 0.2)'
-        ],
-        borderColor: [
-          'rgba(255, 99, 132, 1)',
-          'rgba(54, 162, 235, 1)',
-          'rgba(255, 206, 86, 1)',
-          'rgba(75, 192, 192, 1)',
-          'rgba(153, 102, 255, 1)',
-          'rgba(255, 159, 64, 1)'
-        ],
-        borderWidth: 1,
-      },
-    ],
-  };
+  labeldataBarras: any = [
+    'Lunes',
+    'Martes',
+    'Miercoles',
+    'Jueves',
+    'Viernes',
+    'Sabado',
+    'Domingo',
+  ];
 
-  datosTorta2: ChartData<'doughnut', number[], string> = {
-    labels: ['Jugador', 'Crupier'],
-    datasets: [
-      {
-        label: 'Promedio de jugadas con puntaje 21',
-        data: [65, 59],
-        backgroundColor: [
-          'rgba(255, 99, 132, 0.2)',
-          'rgba(54, 162, 235, 0.2)',
-          'rgba(255, 206, 86, 0.2)',
-          'rgba(75, 192, 192, 0.2)',
-          'rgba(153, 102, 255, 0.2)',
-          'rgba(255, 159, 64, 0.2)'
-        ],
-        borderColor: [
-          'rgba(255, 99, 132, 1)',
-          'rgba(54, 162, 235, 1)',
-          'rgba(255, 206, 86, 1)',
-          'rgba(75, 192, 192, 1)',
-          'rgba(153, 102, 255, 1)',
-          'rgba(255, 159, 64, 1)'
-        ],
-        borderWidth: 1,
-      },
-    ],
-  };
-  
-  constructor() { }
+  labelDataJugador: any = [
+    'jugador',
+    'Crupier',
+  ];
+
+
+
+  constructor(private reportesService: ReportesService) {}
 
   ngOnInit(): void {
+    this.cargarDataRepo1();
+    this.cargarDataRepo2();
+    this.cargarDataRepo3();
+
   }
 
+  cargarDataRepo1() {
+    this.reportesService.getJugadasPorDia().subscribe((data: any) => {
+      this.charDataBarras = data;
+      this.cargarReportes(this.labeldataBarras, this.charDataBarras);
+    });
+  }
+
+  cargarDataRepo2(){
+    this.reportesService.getVictoriaNormales().subscribe((data: any) => {
+      this.charDataVictorias = data;
+      this.cargarReportes2(this.labelDataJugador, this.charDataVictorias);
+    });
+  }
+
+  cargarDataRepo3(){
+    this.reportesService.getVictoriaJugador21().subscribe((data: any) => {
+      this.charDataVictorias21 = data;
+      this.cargarReportes3(this.labelDataJugador, this.charDataVictorias21);
+    });
+  }
+
+  cargarReportes(labeldata: any, charDataBarras: any) {
+    const myChart = new Chart('barras', {
+      type: 'bar',
+      data: {
+        labels: labeldata,
+        datasets: [
+          {
+            label: '# of Votes',
+            data: charDataBarras,
+            backgroundColor: [
+              'rgba(255, 99, 132, 0.2)',
+              'rgba(54, 162, 235, 0.2)',
+              'rgba(255, 206, 86, 0.2)',
+              'rgba(75, 192, 192, 0.2)',
+              'rgba(153, 102, 255, 0.2)',
+              'rgba(255, 159, 64, 0.2)',
+            ],
+            borderColor: [
+              'rgba(255, 99, 132, 1)',
+              'rgba(54, 162, 235, 1)',
+              'rgba(255, 206, 86, 1)',
+              'rgba(75, 192, 192, 1)',
+              'rgba(153, 102, 255, 1)',
+              'rgba(255, 159, 64, 1)',
+            ],
+            borderWidth: 1,
+          },
+        ],
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true,
+          },
+        },
+      },
+    });
+  }
+
+  cargarReportes2(labeldata: any, charDataVictorias: any) {
+    const myChart2 = new Chart('pieVictorias', {
+      type: 'pie',
+      data: {
+        labels: labeldata,
+        datasets: [
+          {
+            label: '# of Votes',
+            data: charDataVictorias,
+            backgroundColor: [
+              'rgba(255, 99, 132, 0.2)',
+              'rgba(54, 162, 235, 0.2)',
+              'rgba(255, 206, 86, 0.2)',
+              'rgba(75, 192, 192, 0.2)',
+              'rgba(153, 102, 255, 0.2)',
+              'rgba(255, 159, 64, 0.2)',
+            ],
+            borderColor: [
+              'rgba(255, 99, 132, 1)',
+              'rgba(54, 162, 235, 1)',
+              'rgba(255, 206, 86, 1)',
+              'rgba(75, 192, 192, 1)',
+              'rgba(153, 102, 255, 1)',
+              'rgba(255, 159, 64, 1)',
+            ],
+            borderWidth: 1,
+          },
+        ],
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true,
+          },
+        },
+      },
+    });
+  }
+
+  cargarReportes3(labeldata: any, charDataVictorias21: any) {
+    const myChart3 = new Chart('pieVictorias21', {
+      type: 'pie',
+      data: {
+        labels: labeldata,
+        datasets: [
+          {
+            label: '# of Votes',
+            data: charDataVictorias21,
+            backgroundColor: [
+              'rgba(255, 99, 132, 0.2)',
+              'rgba(54, 162, 235, 0.2)',
+              'rgba(255, 206, 86, 0.2)',
+              'rgba(75, 192, 192, 0.2)',
+              'rgba(153, 102, 255, 0.2)',
+              'rgba(255, 159, 64, 0.2)',
+            ],
+            borderColor: [
+              'rgba(255, 99, 132, 1)',
+              'rgba(54, 162, 235, 1)',
+              'rgba(255, 206, 86, 1)',
+              'rgba(75, 192, 192, 1)',
+              'rgba(153, 102, 255, 1)',
+              'rgba(255, 159, 64, 1)',
+            ],
+            borderWidth: 1,
+          },
+        ],
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true,
+          },
+        },
+      },
+    });
+  }
 }
--- src/app/services/reportes.service.ts
+++ src/app/services/reportes.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -8,5 +9,23 @@ export class ReportesService {
 
   constructor(private http:HttpClient) { }
 
+  APIUrl = "http://localhost:8080/reportes/";
   
+
+  getVictoriaNormales(): Observable<number>{
+    return this.http.get<number>(this.APIUrl + 'victorias');
+  }
+
+
+  getJugadasPorDia(): Observable<any>{
+    const url = this.APIUrl + "juegosPorDia";
+        const headers = { 'Content-Type': 'application/json' };
+        return this.http.get(url, { headers })
+  }
+
+  getVictoriaJugador21(): Observable<number>{
+    return this.http.get<number>(this.APIUrl + 'victorias21');
+  }
+
+
 }
